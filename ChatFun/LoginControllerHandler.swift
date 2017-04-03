323af049264920b91eb4bdd40e36a6a0a3eb76ca
Thang Le <thangle.mi@gmail.com>
2017-04-03 22:32:48 +0700

    updload image to firebase

---
+++ ChatFun/LoginControllerHandler.swift
@@ -0,0 +1,9 @@
+//
+//  LoginControllerHandler.swift
+//  ChatFun
+//
+//  Created by MGX82 on 4/3/17.
+//  Copyright © 2017 ThangLQ. All rights reserved.
+//
+
+import Foundation
---
+++ ChatFun/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ChatFun
+//
+//  Created by MGX82 on 3/31/17.
+//  Copyright © 2017 ThangLQ. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
